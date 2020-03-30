1bd54b1affad0e2a2eee82961dabb2c4455ee92c
Klemen <naltamer14@gmail.com>
2020-03-30 19:43:26 +0200

    add axios

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Axios from 'axios';
 import router from './router';
 import store from './store'
 import Buefy from 'buefy'
@@ -8,6 +9,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import DefaultLayout from './layouts/Default.vue';
 import LandingLayout from './layouts/Landing.vue';
 
+Vue.prototype.$http = Axios;
 Vue.config.productionTip = false;
 
 Vue.component('layout-default', DefaultLayout);
