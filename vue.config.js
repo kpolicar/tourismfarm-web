7d0d5f3911fb5108a6bc4b5ee487cd0564046a4e
Klemen <naltamer14@gmail.com>
2020-04-05 22:22:53 +0200

    vue svg inline loader remove, replaced with vue-svg-loader: dynamic svg source

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
-    config.module
-      .rule("vue")
-      .use("vue-svg-inline-loader")
-      .loader("vue-svg-inline-loader");
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
   },
   css: {
     loaderOptions: {
