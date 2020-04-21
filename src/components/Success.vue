1f85a77578053813f71ea1ad823d6f4c619b0e86
Klemen <naltamer14@gmail.com>
2020-04-21 15:40:36 +0200

    modify inquiry success message

--- src/components/Success.vue
+++ src/components/Success.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    You've made your inquiry successfully.
+  <div class="has-text-centered">
+    <b-icon icon="check" size="is-large"></b-icon>
+    <p>You've made your inquiry successfully.</p>
+    <p>Please await approval and further instructions.</p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+  import {Component, Prop, Vue} from 'vue-property-decorator';
 
   @Component({})
-export default class Success extends Vue {
+  export default class Success extends Vue {
     @Prop()
     record!: object
-}
+  }
 </script>
